cd67295fa4683c1fb3232dfc7b01603c6a452727
baikadamovagauhar <43268693+baikadamovagauhar@users.noreply.github.com>
2020-04-30 13:09:35 +0300

    Update ExitPuzzle.cs

--- Assets/MyScripts/ExitPuzzle.cs
+++ Assets/MyScripts/ExitPuzzle.cs
@@ -2,23 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ExitPuzzle : MonoBehaviour
-{
+public class ExitPuzzle : MonoBehaviour{
     [SerializeField]
     private Transform[] pictures;
     public GameObject puzzleLayer;
     public GameObject self;
     float[] arr = { 0f, 90f, 180f, 270f };
-    public void Update()
-    {
+    public void Update() {
         if (Input.GetMouseButtonDown(0)) {
             puzzleLayer.SetActive(false);
-            for (int i = 0; i < 8; i++)
-            {
+            for (int i = 0; i < 8; i++) {
                 int rnd = UnityEngine.Random.Range(0, 3);
-
-                pictures[i].eulerAngles = new Vector3(0f, 0f, arr[rnd]);
-
+                pictures[i].eulerAngles = new Vector3(0f, 0f, arr[rnd]); 
             }
             self.SetActive(false);
         }
