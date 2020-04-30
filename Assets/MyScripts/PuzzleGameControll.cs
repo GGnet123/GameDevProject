f48d09a699a804a730d212b52deea74e2e9e7f15
baikadamovagauhar <43268693+baikadamovagauhar@users.noreply.github.com>
2020-04-30 13:09:04 +0300

    Update PuzzleGameControll.cs

--- Assets/MyScripts/PuzzleGameControll.cs
+++ Assets/MyScripts/PuzzleGameControll.cs
@@ -4,16 +4,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PuzzleGameControll : MonoBehaviour
-{
+public class PuzzleGameControll : MonoBehaviour{
     [SerializeField]
     private Transform[] pictures;
-
     [SerializeField]
     private GameObject puzzleLayer;
-
     public GameObject exit;
-
     public static bool youWin;
     public Button btn1;
     public Button btn2;
@@ -26,9 +22,7 @@ public class PuzzleGameControll : MonoBehaviour
     public Button btn9;
     int[] arr = { 0, 90, 180, 270 };
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() { 
         btn1.onClick.AddListener(() => rotate(0));
         btn2.onClick.AddListener(() => rotate(1));
         btn3.onClick.AddListener(() => rotate(2));
@@ -44,8 +38,7 @@ public class PuzzleGameControll : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         Debug.Log("rotation1: " + pictures[0].transform.eulerAngles.z);
         Debug.Log("rotation2: " + pictures[1].transform.eulerAngles.z);
         Debug.Log("rotation3: " + pictures[2].transform.eulerAngles.z);
@@ -63,19 +56,14 @@ public class PuzzleGameControll : MonoBehaviour
             (pictures[5].transform.eulerAngles.z > -1 && pictures[5].transform.eulerAngles.z < 1) &&
             (pictures[6].transform.eulerAngles.z > -1 && pictures[6].transform.eulerAngles.z < 1) &&
             (pictures[7].transform.eulerAngles.z > -1 && pictures[7].transform.eulerAngles.z < 1) &&
-            (pictures[8].transform.eulerAngles.z > -1 && pictures[8].transform.eulerAngles.z < 1))
-        {
+            (pictures[8].transform.eulerAngles.z > -1 && pictures[8].transform.eulerAngles.z < 1)) {
             youWin = true;
             
             Debug.Log("You won");
             exit.SetActive(true);
         }
     }
-    void rotate(int i)
-    {
-        Debug.Log("image clicked");
-        
+    void rotate(int i){
         pictures[i].Rotate(0f, 0f, 90f);
-        
     }
 }
